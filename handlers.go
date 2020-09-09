ef49307913b8abff8c3e408f8864de36eb160982
Fabien Bellanger <valentil@gmail.com>
2020-09-09 22:04:30 +0200

    Ajout du streaming

--- handlers.go
+++ handlers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -31,5 +33,47 @@ func (s *server) handlerBigJSON(c echo.Context) error {
 }
 
 func (s *server) handlerBigJSONStream(c echo.Context) error {
-	return c.JSON(http.StatusOK, nil)
+	type User struct {
+		ID        int    `json:"id"`
+		Username  string `json:"username"`
+		Password  string `json:"-"`
+		Lastname  string `json:"lastname"`
+		Firstname string `json:"firstname"`
+	}
+
+	response := c.Response()
+	response.Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
+	response.WriteHeader(http.StatusOK)
+
+	if _, err := io.WriteString(response, "{"); err != nil {
+		return err
+	}
+
+	encoder := json.NewEncoder(response)
+	for i := 0; i < 100000; i++ {
+		if i > 0 {
+			if _, err := io.WriteString(response, ","); err != nil {
+				return err
+			}
+		}
+
+		user := User{
+			ID:        i + 1,
+			Username:  "My Username",
+			Lastname:  "My Lastname",
+			Firstname: "My Firstname",
+		}
+
+		if err := encoder.Encode(user); err != nil {
+			return err
+		}
+
+		i++
+	}
+
+	if _, err := io.WriteString(response, "}"); err != nil {
+		return err
+	}
+
+	return nil
 }
